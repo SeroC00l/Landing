cd7cc42984b28fb7d8060a10ee730b0cacb8a94b
Sero Cool <aleurrea4@gmail.com>
2024-06-29 16:10:36 -0500

    add project manajer

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,57 +1,80 @@
 export const languages = {
-    es: 'Español',
-    en: 'English',
-  };
-  
-  export const defaultLang = 'es';
-  
-  export const ui = {
-    en: {
-        greeting: "Hello world, I'm",
-        aboutMe: "About Me",
-        experience: "+2 years of experience. Software Engineer and Content Creator on Programming from Bogotá, Colombia.",
-        designDevelopment: "Web Design and Development",
-        programmingContent: "Programming Content",
-        projects: "Projects",
-        downloadCV: "Download CV",
-        aboutMeTitle: "About Me",
-        nowMore: "... Now More",
-        aboutMeText: "I have a strong background in web development, mobile applications, and cloud computing, I seek to bring a blend of technical expertise and creative vision to every project I undertake.",
-        attractiveDesign: "Attractive Design",
-        attractiveDesignDescription: "We design web pages that combine stunning aesthetics with exceptional user interfaces. Our focus goes beyond just the visual; we also ensure that each design element is strategically placed to maximize ease of use and intuitive interaction.",
-        optimizedFunctionality: "Optimized Functionality",
-        optimizedFunctionalityDescription: "We ensure that every element of your Landing Page works optimally for a seamless user experience.",
-        analyticsAndContinuousImprovement: "Analytics and Continuous Improvement",
-        analyticsAndContinuousImprovementDescription: "We track detailed performance metrics of your Landing Pages and apply constant improvements to optimize your results.",
-        effectiveConversion: "Effective Conversion",
-        effectiveConversionDescription: "We apply effective and personalized conversion strategies designed to motivate and inspire your visitors to take the desired action on your website. Whether it's completing a form, making a purchase, or subscribing to your newsletter, our strategies are designed to significantly increase your conversion rates.",
-        masterFinanceTitle: "Master Finance - Multiplatform Finance Application",
-        masterFinanceDesc: "Web and mobile application focused on improving users finances, developed using Next.js, Tailwind CSS, and React Native.",
-        niceNailsTitle: "Nice Nails - Landing Page and Online Store",
-        niceNailsDesc: "Manicure services website, including an online store developed using Astro, Svelte, Tailwind CSS, integrated with Mercadopago API.",
-      },
-      es: {
-        greeting: "Hola mundo, soy",
-        aboutMe: "Sobre mí",
-        nowMore: "... Leer Mas",
-        experience: "+2 años de experiencia. Ingeniero de Software y Creador de Contenido sobre Programación de Bogotá, Colombia.",
-        designDevelopment: "Diseño y Desarrollo de Páginas Web",
-        programmingContent: "Contenido de Programación",
-        projects: "Proyectos",
-        downloadCV: "Descargar CV",
-        aboutMeTitle: "Sobre mí",
-        aboutMeText: "Cuento con una sólida experiencia en desarrollo web, aplicaciones móviles y computación en la nube, Busco aportar una combinación de experiencia técnica y visión creativa a cada proyecto que emprendo.",
-        attractiveDesign: "Diseño Atractivo",
-        attractiveDesignDescription: "Diseñamos páginas web que combinan estética impactante con una interfaz de usuario excepcional. Nuestro enfoque no se limita solo a lo visual; también nos aseguramos de que cada elemento del diseño esté estratégicamente ubicado para maximizar la facilidad de uso y la interacción intuitiva.",
-        optimizedFunctionality: "Funcionalidad Optimizada",
-        optimizedFunctionalityDescription: "Nos aseguramos de que cada elemento de tu Landing Page funcione de manera óptima para una experiencia de usuario sin contratiempos.",
-        analyticsAndContinuousImprovement: "Analítica y Mejora Continua",
-        analyticsAndContinuousImprovementDescription: "Realizamos un seguimiento detallado del rendimiento de tus Landing Pages y aplicamos mejoras constantes para optimizar tus resultados.",
-        effectiveConversion: "Conversión Efectiva",
-        effectiveConversionDescription: "Aplicamos estrategias de conversión efectivas y personalizadas que están diseñadas para motivar e inspirar a tus visitantes a tomar la acción deseada en tu sitio web. Ya sea que se trate de completar un formulario, realizar una compra o suscribirse a tu boletín, nuestras estrategias están diseñadas para aumentar de manera significativa tus tasas de conversión.",
-        masterFinanceTitle: "Master Finance - Aplicación Multiplataforma de Finanzas",
-        masterFinanceDesc: "Aplicación web y móvil centrada en mejorar las finanzas de los usuarios, desarrollada utilizando Next.js, Tailwind CSS y React Native.",
-        niceNailsTitle: "Nice Nails - Landing Page y tienda online",
-        niceNailsDesc: "Página web de servicios de manicura, incluyendo tienda online desarrollada usando Astro, Svelte, Tailwind CSS, integración con la API de Mercadopago.",
-      },  
-    } as const;
+  es: "Español",
+  en: "English",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    greeting: "Hello world, I'm",
+    aboutMe: "About Me",
+    experience:
+      "+2 years of experience. Software Engineer and Content Creator on Programming from Bogotá, Colombia.",
+    designDevelopment: "Web Design and Development",
+    programmingContent: "Programming Content",
+    projects: "Projects",
+    downloadCV: "Download CV",
+    aboutMeTitle: "About Me",
+    nowMore: "... Now More",
+    aboutMeText:
+      "I have a strong background in web development, mobile applications, and cloud computing, I seek to bring a blend of technical expertise and creative vision to every project I undertake.",
+    attractiveDesign: "Attractive Design",
+    attractiveDesignDescription:
+      "We design web pages that combine stunning aesthetics with exceptional user interfaces. Our focus goes beyond just the visual; we also ensure that each design element is strategically placed to maximize ease of use and intuitive interaction.",
+    optimizedFunctionality: "Optimized Functionality",
+    optimizedFunctionalityDescription:
+      "We ensure that every element of your Landing Page works optimally for a seamless user experience.",
+    analyticsAndContinuousImprovement: "Analytics and Continuous Improvement",
+    analyticsAndContinuousImprovementDescription:
+      "We track detailed performance metrics of your Landing Pages and apply constant improvements to optimize your results.",
+    effectiveConversion: "Effective Conversion",
+    effectiveConversionDescription:
+      "We apply effective and personalized conversion strategies designed to motivate and inspire your visitors to take the desired action on your website. Whether it's completing a form, making a purchase, or subscribing to your newsletter, our strategies are designed to significantly increase your conversion rates.",
+    masterFinanceTitle: "Master Finance - Multiplatform Finance Application",
+    masterFinanceDesc:
+      "Web and mobile application focused on improving users finances, developed using Next.js, Tailwind CSS, and React Native.",
+    niceNailsTitle: "Nice Nails - Landing Page and Online Store",
+    niceNailsDesc:
+      "Manicure services website, including an online store developed using Astro, Svelte, Tailwind CSS, integrated with Mercadopago API.",
+    projectManagerTitle: "Project Manager - Orginize your projects & tasks",
+    projectManagerDesc:
+      "Fullstack web application that allows you to list and organize your tasks conveniently, developed with Next.js, Tailwind CSS, Shadcn UI, and Supabase as the database",
+  },
+  es: {
+    greeting: "Hola mundo, soy",
+    aboutMe: "Sobre mí",
+    nowMore: "... Leer Mas",
+    experience:
+      "+2 años de experiencia. Ingeniero de Software y Creador de Contenido sobre Programación de Bogotá, Colombia.",
+    designDevelopment: "Diseño y Desarrollo de Páginas Web",
+    programmingContent: "Contenido de Programación",
+    projects: "Proyectos",
+    downloadCV: "Descargar CV",
+    aboutMeTitle: "Sobre mí",
+    aboutMeText:
+      "Cuento con una sólida experiencia en desarrollo web, aplicaciones móviles y computación en la nube, Busco aportar una combinación de experiencia técnica y visión creativa a cada proyecto que emprendo.",
+    attractiveDesign: "Diseño Atractivo",
+    attractiveDesignDescription:
+      "Diseñamos páginas web que combinan estética impactante con una interfaz de usuario excepcional. Nuestro enfoque no se limita solo a lo visual; también nos aseguramos de que cada elemento del diseño esté estratégicamente ubicado para maximizar la facilidad de uso y la interacción intuitiva.",
+    optimizedFunctionality: "Funcionalidad Optimizada",
+    optimizedFunctionalityDescription:
+      "Nos aseguramos de que cada elemento de tu Landing Page funcione de manera óptima para una experiencia de usuario sin contratiempos.",
+    analyticsAndContinuousImprovement: "Analítica y Mejora Continua",
+    analyticsAndContinuousImprovementDescription:
+      "Realizamos un seguimiento detallado del rendimiento de tus Landing Pages y aplicamos mejoras constantes para optimizar tus resultados.",
+    effectiveConversion: "Conversión Efectiva",
+    effectiveConversionDescription:
+      "Aplicamos estrategias de conversión efectivas y personalizadas que están diseñadas para motivar e inspirar a tus visitantes a tomar la acción deseada en tu sitio web. Ya sea que se trate de completar un formulario, realizar una compra o suscribirse a tu boletín, nuestras estrategias están diseñadas para aumentar de manera significativa tus tasas de conversión.",
+    masterFinanceTitle:
+      "Master Finance - Aplicación Multiplataforma de Finanzas",
+    masterFinanceDesc:
+      "Aplicación web y móvil centrada en mejorar las finanzas de los usuarios, desarrollada utilizando Next.js, Tailwind CSS y React Native.",
+    niceNailsTitle: "Nice Nails - Landing Page y tienda online",
+    niceNailsDesc:
+      "Página web de servicios de manicura, incluyendo tienda online desarrollada usando Astro, Svelte, Tailwind CSS, integración con la API de Mercadopago.",
+    projectManagerTitle: "Project Manager - Organiza tus tareas y proyectos",
+    projectManagerDesc:
+      "Aplicación web Fullstack, que permite listar y organizar tus tareas de manera cómoda, desarrollada en Nexts.js, Tailwind CSS, Shadcn ui y supabase como base de datos",
+  },
+} as const;
